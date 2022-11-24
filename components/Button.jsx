06ca182fdd18f7ebd9968025bf9bf363e1fe0bcc
Valdi <rvdans002@gmail.com>
2022-11-24 22:42:42 +0700

    add Button component

---
+++ components/Button.jsx
@@ -0,0 +1,64 @@
+import Link from 'next/link';
+
+const Button = ({
+  text = 'Button Text',
+  type = 'button' || 'link' || 'anchor',
+  theme = 'dark' || 'light' || 'greem',
+  size = 'normal' || 'small',
+  href = '',
+  onClick = (e = new Event()) => {},
+}) => {
+  const baseClass = 'block w-fit rounded-[10px] font-semibold text-center';
+
+  const themeClass = {
+    dark: 'bg-web-color-dark-gray text-white',
+    light: 'bg-white text-brand-footer',
+    green: 'bg-brand-green text-white',
+  };
+
+  const sizeClass = {
+    normal: 'px-[60px] py-[20px] text-lg leading-[1.375rem]',
+    small: 'px-[40px] py-[20px] text-sm',
+  };
+
+  const isTypeButton = type === 'button';
+  const isTypeLink = type === 'link';
+  const isTypeAnchor = type === 'anchor';
+
+  if (isTypeButton) {
+    return (
+      <button
+        onClick={onClick}
+        className={`${baseClass} ${sizeClass[size]} ${themeClass[theme]}`}
+      >
+        {text}
+      </button>
+    );
+  }
+
+  if (isTypeLink) {
+    return (
+      <Link
+        href={href}
+        onClick={onClick}
+        className={`${baseClass} ${sizeClass[size]} ${themeClass[theme]}`}
+      >
+        {text}
+      </Link>
+    );
+  }
+
+  if (isTypeAnchor) {
+    return (
+      <a
+        href={href}
+        onClick={onClick}
+        className={`${baseClass} ${sizeClass[size]} ${themeClass[theme]}`}
+      >
+        {text}
+      </a>
+    );
+  }
+};
+
+export default Button;
